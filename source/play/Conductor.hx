ee1ab0d8902e9079fbf8ea48fbac5500ae74bf9b
SIG7Pro <SIG7isme@gmail.com>
2025-04-07 13:49:37 -0400

    Fixed Crashes + Added "Debug Support"
    Commiting this before I see what I can redo in the Conductor. Turns out it was one line causing the crashes.
    
    https://fizzd.me/posts/how-to-make-a-rhythm-game-a-quick-and-dirty-guide-to-setting-up-your-project I might see what I can do with this too.

--- source/play/Conductor.hx
+++ source/play/Conductor.hx
@@ -2,54 +2,76 @@ package play;
 class Conductor{
 // https://www.reddit.com/r/gamedev/comments/2fxvk4/heres_a_quick_and_dirty_guide_i_just_wrote_how_to/
 // https://www.gamedeveloper.com/audio/coding-to-the-beat---under-the-hood-of-a-rhythm-game-in-unity
-    public var bpm:Float = 100;
-    // BPM of song.
-    public var crotchet:Float = 0;
-    // Duration of a beat calculated from BPM. // BPS
-    public var offset:Float = 0;
-    // Offset likely won't be accounted for in this file as of writing.
-    public var songPosition:Float;
-    // Current song placement. Ex: 102.9 seconds.
-    public var beatNumber:Float;
-    // Current beat.
-    public var lastBeat:Float;
-    // Previous beat.
+    public var bpm:Float; // BPM of song.
+    public var crotchet:Float = 0; // Duration of a beat calculated from BPM. // Beats per second.
 
-    public var amountBumped:Float = 0;
-    // Variable dedicated to how many bumps occured.
+    public var songPosition:Float; // in MS
+    public var songBeatsPosition:Float; // in beats
 
-    public var songActive:Bool = true;
+    public var beatNumber:Float; // Current beat.
+    public var lastBeat:Float; // Previous beat.
+
+    public var amountBumped:Float = 0; // Variable dedicated to how many bumps occured.
+
+    //public var songActive:Bool = true;
     public var isBump:Bool = false; /* Determines if the song is on a beat. */
 
-        public function activate(bpm:Float, songActive:Bool) {
+    public var songLength:Float; // Determines the length of the song. It should be grabbed with the "length" variable of the FlxSound.
 
-            //crotchet = 60 / bpm; // Beats per sec.
-           // if (songActive){ // Requires a loaded FlxSound.
-            //    update(songPosition);
-           // }
+        var inputBPM:Float;
+        public function activate(inputBPM:Float) {
+            bpm = inputBPM;
+            trace("Conductor Activated. BPM: " + bpm);
+            //todo: figure out 4 beat - bump method
         }
 
-        public function new(){
-        } // Required or else there will be issues compiling.
+        public function new(){}Z
 
-        public function updateInfo(songPosition:Float)
+        public function updateInfo(songPosition:Float, songLength:Float)
         {
-            if (songActive)
-            {
-                if (songPosition > lastBeat + crotchet)
+        crotchet = ((60 / bpm) * 1000);
+        songBeatsPosition = songPosition / crotchet;
+
+                if (songBeatsPosition > (lastBeat + crotchet))
                 {
                     bump(); // Bump, a way to signal a beat.
                     lastBeat += crotchet;
+                }/*else if  (songPosition >= (songLength - 999)){ // Remember, the song's length and positions are declared via miliseconds.
+                    songDone();
+                }*/
+                if (songPosition == 0)
+                {
+                    songDone();
+                    return;
                 }
-            }
+
+
+
 
         }
 
+        public function bumpold(){
+
+            if (amountBumped < 4){ // Apparently most songs go by beats of 4.
+                trace("Bump!");
+                amountBumped += 1;
+                isBump = false;
+            }else if (amountBumped >= 4){
+                isBump = true;
+                amountBumped = 0;
+            }
+        }
+
         public function bump(){
-            isBump = true;
-            trace("Bump!");
-            amountBumped += 1;
-            isBump = false;
+            if (amountBumped < 4){ // Apparently most songs go by beats of 4.
+                amountBumped += 1;
+                trace("Hold");
+                isBump = false;
+            }else if (amountBumped >= 4){
+                amountBumped = 0;
+                trace("Bump!");
+                isBump = true;
+            }
         }
 
         public function songDone(){
--- source/play/PlayState.hx
+++ source/play/PlayState.hx
@@ -8,79 +8,101 @@ import flixel.util.FlxColor;
 
 import flixel.sound.FlxSound;
 import play.Conductor;
-// Shoutouts to formatter.org. Formatted with Chromium Style with an Indent of 5. Though I did move some variables and functions back one.
-	class PlayState extends FlxState {
-	var colorLocation : String = "assets/images/colors/";
+class PlayState extends FlxState {
+		var colorLocation : String = "assets/images/colors/";
 
-	var test:FlxSprite;
-	var mainFocus:FlxSprite;
-	var songConductor:Conductor;
-	var sprite:FlxSprite;
-	var myText:FlxText;
+		var test:FlxSprite;
+		var mainFocus:FlxSprite;
+		var songConductor:Conductor;
+		var sprite:FlxSprite;
+		var infoText:FlxText;
 
-	// Ref: https://youtu.be/qR1OntJJVKk?t=349
-	var currentSong:FlxSound;
+		// Ref: https://youtu.be/qR1OntJJVKk?t=349
+		var currentSong:FlxSound;
 
-	var songTime:Float;
+		var songTime:Float;
 
-	override public function create():Void {
-          mainFocus = new FlxSprite("assets/images/finder.png");
-          // mainFocus.loadGraphic("assets/images/finder.png");
-          mainFocus.scale.x = 0.6;
-          mainFocus.scale.y = 0.6;
-          mainFocus.updateHitbox();
-          mainFocus.screenCenter();
-          add(mainFocus);
+		var songName:String;
+		var songBeats:Float = 100; //BPM
+		var inputBPM:Float; // BPM Input?
 
-          sprite = new FlxSprite();
-          sprite.makeGraphic(128, 64, FlxColor.BLUE);
-          sprite.screenCenter();
-          // add(sprite);
-
-          myText = new FlxText(12, 12, FlxG.width - 8, "", 12);
-          myText.text = "Test State";
-          myText.color = FlxColor.WHITE;
-          myText.setBorderStyle(OUTLINE, 0xFF38348e, 1);
-          myText.font = "assets/fonts/Fairfax.ttf";
-          add(myText);
-
-          trace("Sprites Made");
-
-          // BPM: 125
-
-          if (FlxG.sound.music == null) {
-                startSong("HE.ogg", 125); // Assumes its in "/assets/music" already.
-					trace("Song Loaded?");
-				//songConductor.activate(bpm, true);
-					trace("Conductor.");
-         }
-	}
-
-	override function update(elapsed : Float) : Void {
-          // myText.text =
-         /* if (songConductor.isBump == false){
-				mainFocus.alpha = 0.5;
-          }else if (songConductor.isBump == true){
-				mainFocus.alpha = 1;
-          }*/
+		override public function create():Void {
+			mainFocus = new FlxSprite("assets/images/finder.png");
+			// mainFocus.loadGraphic("assets/images/finder.png");
+			mainFocus.scale.x = 0.6;
+			mainFocus.scale.y = 0.6;
+			mainFocus.updateHitbox();
+			mainFocus.screenCenter();
 
 
-		 //songConductor.songPosition = currentSong.time;
-         // songTime = currentSong.time;
-         // songConductor.update(songTime);
-         //songConductor.updateInfo(currentSong.time);
+			sprite = new FlxSprite();
+			sprite.makeGraphic(256, 128, FlxColor.BLUE);
+			sprite.screenCenter();
+			sprite.alpha = 0.25;
+			add(sprite);
 
-	}
+			add(mainFocus);
 
-	var songName:String;
-	var bpm:Float;
-	function startSong(songName:String, bpm:Float) {
-          // FlxG.sound.load stores a sound.
-          // 		  .play("..."); plays a sound.
-          //		 	  .playMusic("..."); Apparently plays music that loops. Relates to FlxG.sound.music. sound = new FlxSound;
-          if (currentSong == null)
-               currentSong = FlxG.sound.load("assets/music/" + songName);
-          currentSong.play();
+			infoText = new FlxText(12, 12, FlxG.width - 8, "", 12);
+			infoText.text = "Test State";
+			infoText.color = FlxColor.WHITE;
+			infoText.setBorderStyle(OUTLINE, 0xFF38348e, 1);
+			infoText.font = "assets/fonts/Fairfax.ttf";
+			add(infoText);
 
-	}
+			songConductor = new Conductor();
+			//add(songConductor);
+
+			trace("Sprites Made");
+
+			// BPM: 125
+
+			if (FlxG.sound.music == null) {
+					startSong("HE.ogg", 125); // Assumes its in "/assets/music" already.
+					trace("Song Loaded.");
+			}
+		}
+
+		override function update(elapsed:Float):Void {
+		// 0/10000
+		// BPM: 100 | Chrotchet: 0.1
+		// Last Beat / Current Beat
+			infoText.text = currentSong.time + " / " + currentSong.length +
+			"\n(Position in Beats: " + songConductor.songBeatsPosition + " | Current Beat: " + songConductor.beatNumber + ")" +
+			"\nBPM: " + songBeats + " (In) " + songConductor.bpm + " | Chrotchet: " + songConductor.crotchet +
+			"\nLast Beat: " + songConductor.lastBeat + "\nAmount Bumped: " + songConductor.amountBumped;
+
+			if (FlxG.sound.music != null) {
+			trace("Music not null.");
+				if (songConductor.isBump == false){
+						mainFocus.alpha = 0.5;
+						mainFocus.scale.set(1.5, 1.5);
+						trace("Ah!");
+				}else if (songConductor.isBump == true){
+						mainFocus.alpha = 1;
+						mainFocus.scale.set(1.0, 1.0);
+				}
+			}
+
+
+			//songConductor.songPosition = currentSong.time;
+			// songTime = currentSong.time;
+			// songConductor.update(songTime);
+			songConductor.updateInfo(currentSong.time, currentSong.length);
+
+		}
+
+		function startSong(songName:String, inputBPM:Float){
+			// FlxG.sound.load stores a sound.
+			// 		  .play("..."); plays a sound.
+			//		 	  .playMusic("..."); Apparently plays music that loops. Relates to FlxG.sound.music. sound = new FlxSound;
+			if (currentSong == null)
+				currentSong = FlxG.sound.load("assets/music/" + songName);
+			currentSong.play();
+			songBeats = inputBPM;
+			trace('BPM: $songBeats.');
+			songConductor.activate(songBeats);
+			trace("PlayState Conductor.");
+
+		}
 }
